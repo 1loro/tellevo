6c5e72a84107af25d646990cfb16afc72ba2c680
lorenzo <134718943+1loro@users.noreply.github.com>
2024-11-14 22:02:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
       mobileNav.classList.toggle('active');
     });
   
-    // Close mobile menu when clicking on a link
+   
     const mobileLinks = mobileNav.querySelectorAll('a');
     mobileLinks.forEach(link => {
       link.addEventListener('click', () => {
@@ -16,3 +16,45 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    const lorenzoImage = document.querySelector('.student-card:nth-child(1) .student-image');
+    const gasparImage = document.querySelector('.student-card:nth-child(2) .student-image');
+
+    const audioLorenzo = document.getElementById('audioLorenzo');
+    const audioGaspar = document.getElementById('audioGaspar');
+
+
+    audioLorenzo.volume = 0.2; 
+    audioGaspar.volume = 0.2;   
+
+
+    lorenzoImage.addEventListener('mouseenter', function() {
+
+        if (!audioLorenzo.paused) {
+            audioLorenzo.pause();
+        }
+
+        audioLorenzo.play();
+    });
+
+
+    gasparImage.addEventListener('mouseenter', function() {
+
+        if (!audioGaspar.paused) {
+            audioGaspar.pause();
+        }
+
+        audioGaspar.play();
+    });
+
+
+    lorenzoImage.addEventListener('mouseleave', function() {
+        audioLorenzo.pause();  
+    });
+
+
+    gasparImage.addEventListener('mouseleave', function() {
+        audioGaspar.pause();  
+    });
+});
+
